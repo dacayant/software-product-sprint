979e0bc8b16b29bd7d367187754cbc6ed1baad0e
Tristram Dacayan <dacayant@kean.edu>
2021-06-10 03:20:54 +0000

    Change "Hello World" to "Hello "Tristram Dacayan"
    Fix serialization error

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -10,9 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+  public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Tristram Dacayan!</h1>");
   }
 }
