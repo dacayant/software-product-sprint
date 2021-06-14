b7c50434daeb7adb4147ff69c7ffc1803f68702b
Tristram Dacayan <dacayant@kean.edu>
2021-06-14 02:35:59 +0000

    Add Contact.js to change form visibility when condition met

---
+++ portfolio/src/main/webapp/contact-holder.js
@@ -0,0 +1,12 @@
+function disableOption() {
+    /*
+    If email checked
+        enable email
+    If phone checked
+        enable phone
+    else
+        no nothing
+    */
+
+    var isPhone = document.getElementsByName("phone-opt")
+}
